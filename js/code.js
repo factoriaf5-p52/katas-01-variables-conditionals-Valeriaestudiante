b933eb887ef335b534aec405e1b883bc4c43b4a6
Valeriaestudiante <116070678+Valeriaestudiante@users.noreply.github.com>
2022-11-12 15:49:05 +0100

    katas 1 de valeria

--- js/code.js
+++ js/code.js
@@ -1,7 +1,14 @@
-function evenOrOdd(n) {
-    return "";
+function evenOrOdd (n){
+    if (n % 2 === 0) {
+        return "even";
+    } if (n % 2 === 1){
+        return "odd";
+    }
 }
 
+
 function greeting(name) {
-    return "";
+    const saludo = "Hola, ";
+    name== "Javi";"gente";"Mary"
+    return saludo + name + "!";
 }
--- js/tests.js
+++ js/tests.js
@@ -9,12 +9,12 @@ describe('Las variables en Javascript', () => {
   })
   it('pueden contener números', () => {
     // Añade la línea que falta
-
+    const b = 14.64; 
     expect(b).to.equal(14.64);
   })
   it('pueden contener booleanos', () => {
     // Añade la línea que falta
-
+    const c = false;
     expect(c).to.equal(false);
   })
 })
@@ -22,15 +22,15 @@ describe('Las variables en Javascript', () => {
 describe('Comparaciones en Javascript', () => {
     it("usando ===", () => {
         const number = 20;
+        const expresion = number === 13;
         //completa la asignación usando number y el comparador ===
-
         expect(expresion).to.equal(false)
     })
 
     it("usando !==", () => {
         const language = "Javascript";
         //completa la asignación usando language y el comparador !==
-
+        const expresion = language !== "Javascript";
         expect(expresion).to.equal(false)
     })
 })
@@ -38,16 +38,19 @@ describe('Comparaciones en Javascript', () => {
 describe("Operaciones aritméticas", () => {
     it("Usando suma", () => {
         const a = 10;
+        const b = 90;
 
         expect(a+b).to.equal(100);
     })
     it("Usando multiplicación", () => {
         const a = 10;
+        const b = 1.3;
 
         expect(a*b).to.equal(13);
     })
     it("Usando división", () => {
         const a = 10;
+        const b = 4;
 
         expect(a/b).to.equal(2.5);
     })
@@ -56,6 +59,7 @@ describe("Operaciones aritméticas", () => {
 describe('Algebra booleana', () => {
     it("Usando el operador AND (&&)", () => {
         const a = true;
+        const b = true;
         //completa la asignación de b
 
         const expression = a && b;
@@ -63,6 +67,7 @@ describe('Algebra booleana', () => {
     })
     it("Usando el operador OR (||)", () => {
         const a = false;
+        const b = false;
         //completa la asignación de b
 
         const expression = a || b;
